d58b735eca96d0ae35e8613fbe73796323eafd0e
specialprog <tr2prog@bellsouth.net>
2024-07-01 15:10:24 -0400

    SOLUTION

--- src/app/core/services/data/color.service.ts
+++ src/app/core/services/data/color.service.ts
@@ -1,8 +1,10 @@
 import { Injectable, inject } from '@angular/core';
 import { ApiService } from './api.service';
-import { BehaviorSubject, Observable, combineLatest, map, zip } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest, concatMap, from, map, toArray, zip } from 'rxjs';
 import { NamedColor } from '../../../shared/data/named-color.model';
-import { sortObs } from '../../../shared/utilities/pipeable-operators';
+import { addGroup, addRGBValues, addTextColor, sortObs } from '../../../shared/utilities/pipeable-operators';
+import { isValidB, isValidG, isValidHexValue, isValidR } from '../../../shared/utilities/color-util';
+import { fuzzy } from '../../../shared/utilities/basic-util';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +17,34 @@ export class ColorService {
   public filterText$: Observable<string> = this.filterTextSubject.asObservable().pipe(
     map(text => text.trim()),
   );
+  
+  returnAllColors$: Observable<boolean> = this.filterText$.pipe(
+    map((filterText: string) => filterText === ''),
+  )
+
+  filterByHex$: Observable<boolean> = this.filterText$.pipe(
+    map((filterText: string) => isValidHexValue(filterText)),
+  )
+
+  filterByRValue$: Observable<boolean> = this.filterText$.pipe(
+    map((filterText: string) => isValidR(filterText))
+  )
+
+  filterByGValue$: Observable<boolean> = this.filterText$.pipe(
+    map((filterText: string) => isValidG(filterText))
+  )
+
+  filterByBValue$: Observable<boolean> = this.filterText$.pipe(
+    map((filterText: string) => isValidB(filterText))
+  )
+
+  filterByGroup$: Observable<boolean> = this.filterText$.pipe(
+    map((filterText: string) => filterText.toLocaleLowerCase().includes('group:'))
+  )
+
+  filterByFuzzy$: Observable<boolean> = this.filterText$.pipe(
+    map((filterText: string) => filterText.startsWith('~'))
+  )
 
   // Modify this pipeline to add the group, rValue, gValue, bValue and textColor
   // properties to each NamedColor object in the array
@@ -23,6 +53,11 @@ export class ColorService {
   // You can pass 'white' and 'black' as the lightColor and darkColor parameters, respectively.
 
   colorsWithInfo$: Observable<NamedColor[]> = this.apiService.allNamedColors$.pipe(
+    concatMap((allColors: NamedColor[]) => from(allColors)),
+    addGroup(),
+    addRGBValues(),
+    addTextColor(),
+    toArray(),
     sortObs<NamedColor>('name'),
   )
 
@@ -31,15 +66,50 @@ export class ColorService {
 
   // Use the fuzzy function to do a fuzzy search of the name property.
   filterBy$ = zip(
+    this.returnAllColors$,
+    this.filterByHex$,
+    this.filterByRValue$,
+    this.filterByGValue$,
+    this.filterByBValue$,
+    this.filterByGroup$,
+    this.filterByFuzzy$
   )
 
   filteredColors$: Observable<NamedColor[]> = combineLatest([
     this.colorsWithInfo$,
     this.filterText$,
-    //this.filterBy$
+    this.filterBy$
   ]).pipe(
-    map(([allColors, filterText]: [NamedColor[], string]) => {
-      return allColors;
+    map(([allColors, filterText, [returnAll, byHex, byR, byG, byB, byGroup, byFuzzy]]: [NamedColor[], string, [boolean, boolean, boolean, boolean, boolean, boolean, boolean]]) => {
+      if (returnAll) {
+        return allColors;
+      }
+
+      if (byHex) {
+        return allColors.filter((color: NamedColor) => color.hexValue.toLocaleLowerCase() === filterText.toLocaleLowerCase());
+      }
+
+      if (byR) {
+        return allColors.filter((color: NamedColor) => color.rValue === +filterText.toLocaleLowerCase().replace('r', ''));
+      }
+
+      if (byG) {
+        return allColors.filter((color: NamedColor) => color.gValue === +filterText.toLocaleLowerCase().replace('g', ''));
+      }
+
+      if (byB) {
+        return allColors.filter((color: NamedColor) => color.bValue === +filterText.toLocaleLowerCase().replace('b', ''));
+      }
+
+      if (byGroup) {
+        return allColors.filter((color: NamedColor) => color.group?.toLocaleLowerCase() === filterText.toLocaleLowerCase().replace('group:', '').trim());
+      }
+
+      if (byFuzzy) {
+        return allColors.filter((color: NamedColor) => fuzzy(color.name.toLocaleLowerCase() ,filterText.toLocaleLowerCase().replace('~', '')));
+      }
+
+      return allColors.filter((color: NamedColor) => color.name.toLocaleLowerCase().includes(filterText.toLocaleLowerCase()));
     } )
   )
 
